f489feff5075e6a462153b278185111e9b5994b6
Nikita Kozlov <Nyarstot@protonmail.com>
2022-12-11 01:01:36 +0300

    add repo info widget to git timeline

--- Shyosai/Widgets/Timeline.py
+++ Shyosai/Widgets/Timeline.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.insert(0, 'C:/Users/winte/source/repos/Nyarstot/GitShyosai')
 
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import Qt
 
 from PyQt5.QtWidgets import QHeaderView
 from PyQt5.QtWidgets import QApplication
@@ -12,14 +12,35 @@ from PyQt5.QtWidgets import QAbstractItemView
 from Shyosai.Utils.ShyosaiDateTime import ShyosaiDateTime
 
 
-class ShyosaiTimeline(QTableWidget):
+class GitGraphWidget(QTableWidget):
+
+    def __init__(self, a_sBranches:list=[], a_stParent=None)->None:
+        super(GitGraphWidget, self).__init__(a_stParent)
+        self.__m_sBranches = a_sBranches
+
+        self.setRowCount(1)
+        self.setColumnCount(2)
+        self.verticalHeader().setVisible(False)
+        self.horizontalHeader().setVisible(False)
+        self.horizontalHeader().setStretchLastSection(True)
+        self.verticalHeader().setDefaultSectionSize(500)
+
+        self.__m_stTimeline = Timeline(self.__m_sBranches)
+        self.__m_stRepoInfo = RepoInfoItemWidget(a_sBranches=self.__m_sBranches)
+
+        self.setCellWidget(0, 0, self.__m_stRepoInfo)
+        self.setCellWidget(0, 1, self.__m_stTimeline)
+
+
+class Timeline(QTableWidget):
 
     def __init__(self, a_sBranches:list=[], a_stParent=None)-> None:
-        super(ShyosaiTimeline, self).__init__(a_stParent)
+        super(Timeline, self).__init__(a_stParent)
         self.__m_sBranches = a_sBranches
 
         # self.horizontalHeader().setDefaultSectionSize(1200)
         self.verticalHeader().setDefaultSectionSize(500)
+        self.verticalHeader().setVisible(False)
         self.setHorizontalScrollMode(QAbstractItemView.ScrollMode.ScrollPerPixel)
         self.setShowGrid(False)
 
@@ -69,20 +90,34 @@ class MonthItemWidget(QTableWidget):
         return total_width
 
 
-class InfoItemWidget(QTableWidget):
+class RepoInfoItemWidget(QTableWidget):
 
-    def __init__(self, a_sBranches:list=[], a_stParent=None):
-        super(InfoItemWidget, self).__init__(a_stParent)
+    def __init__(self, a_sRepoName:str='Untitled', a_sBranches:list=[], a_stParent=None):
+        super(RepoInfoItemWidget, self).__init__(a_stParent)
+        self.__m_sRepoName = a_sRepoName
         self.__m_sBranches = a_sBranches
 
-    def __draw_timeline(self):
-        self.setRowCount(len(self.__m_sBranches))
         self.setColumnCount(1)
+        self.verticalHeader().setVisible(False)
+        self.horizontalHeader().setFixedHeight(47)
+        self.horizontalHeader().setStretchLastSection(True)
+        self.setHorizontalHeaderItem(0, QTableWidgetItem(self.__m_sRepoName))
+
+        self.__init_ui()
+
+    def __init_ui(self):
+        self.setRowCount(len(self.__m_sBranches))
+
+        for i in range(self.rowCount()):
+            item = QTableWidgetItem(self.__m_sBranches[i])
+            item.setTextAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
+            self.setItem(i, 0, item)
 
 
 if __name__ == "__main__":
     branches = ['master', 'dev', 'old-state']
     app = QApplication(sys.argv)
-    gui = ShyosaiTimeline(branches)
+    gui = GitGraphWidget(branches)
     gui.show()
     app.exec_()
+
